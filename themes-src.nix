65e4046914dea2b4438c2dbd9717777b9c77fa0c
pietdevries94 <piet@devries.tech>
2020-11-24 21:28:23 +0100

    Update themes dependency

--- themes-src.nix
+++ themes-src.nix
@@ -4,6 +4,7 @@ in
   pkgs.fetchFromGitHub {
     owner = "morpheusthewhite";
     repo = "spicetify-themes";
-    rev = "443cc26ecec7aa32921a0ba951d1159ac5b48a19";
-    sha256 = "04v32s4pq54mvq2pfm8l1511dwijplgw7axdd9fjax25c0rczj62";
+    rev = "fdadc4c1cfe38ecd22cf828d2c825e0af1dcda9f";
+    sha256 = "1k44g8rmf8bh4kk16w4n9z1502ag3w67ad3jx28327ykq8pq5w29";
+    fetchSubmodules = true;
   }
