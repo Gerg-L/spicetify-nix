677270cc4d453382ef33af85360c9430014c8ab8
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-06-13 01:24:01 -0500

    cleanup and move to GPL3.0

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A nix flake that provides a home-manager module to configure spicetify with.";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    spicetify-themes = {
+        url = "github:morpheusthewhite/spicetify-themes";
+        flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, spicetify-themes, ... }@inputs:
+    {
+      homeManagerModule = import ./module.nix self;
+    };
+}
--- module.nix
+++ module.nix
@@ -1,8 +1,9 @@
-{ lib, pkgs, config, ... }:
-with lib;                      
+{ lib, pkgs, config, spicetify-themes, ... }:
+with lib;
 let
   cfg = config.programs.spicetify;
-in {
+in
+{
   options.programs.spicetify = {
     enable = mkEnableOption "A modded Spotify";
     theme = mkOption {
@@ -15,23 +16,23 @@ in {
     };
     thirdParyThemes = mkOption {
       type = types.attrs;
-      default = {};
+      default = { };
     };
     thirdParyExtensions = mkOption {
       type = types.attrs;
-      default = {};
+      default = { };
     };
     thirdParyCustomApps = mkOption {
       type = types.attrs;
-      default = {};
+      default = { };
     };
     enabledExtensions = mkOption {
       type = types.listOf types.str;
-      default = [];
+      default = [ ];
     };
     enabledCustomApps = mkOption {
       type = types.listOf types.str;
-      default = [];
+      default = [ ];
     };
     spotifyLaunchFlags = mkOption {
       type = types.str;
@@ -104,36 +105,116 @@ in {
   };
 
   config = mkIf cfg.enable {
-    home.packages = [
-      (pkgs.callPackage ./package.nix {
-        inherit pkgs;
-        inherit (cfg) 
-          theme
-          colorScheme
-          thirdParyThemes
-          thirdParyExtensions
-          thirdParyCustomApps
-          enabledExtensions
-          enabledCustomApps
-          spotifyLaunchFlags
-          injectCss
-          replaceColors
-          overwriteAssets
-          disableSentry
-          disableUiLogging
-          removeRtlRule
-          exposeApis
-          disableUpgradeCheck
-          fastUserSwitching
-          visualizationHighFramerate
-          radio
-          songPage
-          experimentalFeatures
-          home
-          lyricAlwaysShow
-          lyricForceNoSync
-        ;
-      })
-    ];
+    # install necessary packages for this user
+    home.packages =
+      let
+        inherit (pkgs.lib.lists) foldr;
+        inherit (pkgs.lib.attrsets) mapAttrsToList;
+
+        # Helper functions
+        pipeConcat = foldr (a: b: a + "|" + b) "";
+        lineBreakConcat = foldr (a: b: a + "\n" + b) "";
+        boolToString = x: if x then "1" else "0";
+        makeLnCommands = type: (mapAttrsToList (name: path: "ln -sf ${path} ./${type}/${name}"));
+        # Setup spicetify and themes
+        spicetify = "SPICETIFY_CONFIG=. ${pkgs.spicetify-cli}/spicetify";
+        themes = spicetify-themes;
+
+        # Dribblish is a theme which needs a couple extra settings
+        isDribblish = theme == "Dribbblish";
+
+        extraCommands = (if isDribblish then "cp ./Themes/Dribbblish/dribbblish.js ./Extensions \n" else "")
+          + (lineBreakConcat (makeLnCommands "Themes" thirdParyThemes))
+          + (lineBreakConcat (makeLnCommands "Extensions" thirdParyExtensions))
+          + (lineBreakConcat (makeLnCommands "CustomApps" thirdParyCustomApps));
+
+        customAppsFixupCommands = lineBreakConcat (makeLnCommands "Apps" thirdParyCustomApps);
+
+        injectCssOrDribblish = boolToString (isDribblish || injectCss);
+        replaceColorsOrDribblish = boolToString (isDribblish || replaceColors);
+        overwriteAssetsOrDribblish = boolToString (isDribblish || overwriteAssets);
+
+        extensionString = pipeConcat ((if isDribblish then [ "dribbblish.js" ] else [ ]) ++ enabledExtensions);
+        customAppsString = pipeConcat enabledCustomApps;
+        
+        # custom spotify package with spicetify integrated in
+        spiced-spotify-unwrapped = pkgs.spotify-unwrapped.overrideAttrs (oldAttrs: rec {
+          postInstall = ''
+            touch $out/prefs
+            mkdir Themes
+            mkdir Extensions
+            mkdir CustomApps
+
+            find ${themes} -maxdepth 1 -type d -exec ln -s {} Themes \;
+            ${extraCommands}
+    
+            ${spicetify} config \
+              spotify_path "$out/share/spotify" \
+              prefs_path "$out/prefs" \
+              current_theme ${theme} \
+              ${if 
+                  colorScheme != ""
+                then 
+                  ''color_scheme "${colorScheme}" \'' 
+                else 
+                  ''\'' }
+              ${if 
+                  extensionString != ""
+                then 
+                  ''extensions "${extensionString}" \'' 
+                else 
+                  ''\'' }
+              ${if
+                  customAppsString != ""
+                then 
+                  ''custom_apps "${customAppsString}" \'' 
+                else 
+                  ''\'' }
+              ${if
+                  spotifyLaunchFlags != ""
+                then 
+                  ''spotify_launch_flags "${spotifyLaunchFlags}" \'' 
+                else 
+                  ''\'' }
+              inject_css ${injectCssOrDribblish} \
+              replace_colors ${replaceColorsOrDribblish} \
+              overwrite_assets ${overwriteAssetsOrDribblish} \
+              disable_sentry ${boolToString disableSentry } \
+              disable_ui_logging ${boolToString disableUiLogging } \
+              remove_rtl_rule ${boolToString removeRtlRule } \
+              expose_apis ${boolToString exposeApis } \
+              disable_upgrade_check ${boolToString disableUpgradeCheck } \
+              fastUser_switching ${boolToString fastUserSwitching } \
+              visualization_high_framerate ${boolToString visualizationHighFramerate } \
+              radio ${boolToString radio } \
+              song_page ${boolToString songPage } \
+              experimental_features ${boolToString experimentalFeatures } \
+              home ${boolToString home } \
+              lyric_always_show ${boolToString lyricAlwaysShow } \
+              lyric_force_no_sync ${boolToString lyricForceNoSync }
+
+            ${spicetify} backup apply
+
+            cd $out/share/spotify
+            ${customAppsFixupCommands}
+          '';
+        });
+      in
+      [
+        spiced-spotify-unwrapped
+        pkgs.spicetify-cli
+      ];
+
+    # install themes into configuration directory
+    home.file = {
+      ".config/spicetify/Themes" = {
+        source = spicetify-themes;
+        recursive = true;
+      };
+    };
+
+    # create postInstall script which will set up spotify according to prefs
+
   };
 }
+
