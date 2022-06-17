ab4a7cf65829eea69e6dea555f6d0dbdbee6ad27
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-06-17 17:12:56 -0500

    removed trace, now remove newlines

--- module.nix
+++ module.nix
@@ -202,9 +202,10 @@ in
         });
         
         # turn the ini file into a bunch of append commands
-        config-xpui-split = lib.lists.forEach (lib.strings.splitString "\n" config-xpui) (string: builtins.trace string string);
-        config-xpui-commands = builtins.concatStringsSep "\n"
-            (map (str: "echo \"${str}\" >> config-xpui.ini") (builtins.trace config-xpui-split config-xpui-split));
+        config-xpui-split = lib.strings.splitString "\n" config-xpui;
+        config-xpui-commands-split = lib.lists.remove "\n"
+            (map (str: "echo \"${str}\" >> config-xpui.ini") config-xpui-split);
+        config-xpui-commands = builtins.concatStringsSep "\n"  config-xpui-commands-split;
 
         # INI created, now create the postInstall that runs spicetify
 
