51f7e87800c7ef703ff0e59123eb4524f609b4f5
Gerg-L <GregLeyda@proton.me>
2023-10-28 10:23:56 -0400

    fix flake-compat

--- default.nix
+++ default.nix
@@ -2,11 +2,17 @@
   (
     let
       lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+      nodeName = lock.nodes.root.inputs.flake-compat;
     in
     fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-      sha256 = lock.nodes.flake-compat.locked.narHash;
+      url =
+        lock.nodes.${nodeName}.locked.url
+          or "https://github.com/edolstra/flake-compat/archive/${
+            lock.nodes.${nodeName}.locked.rev
+          }.tar.gz";
+      sha256 = lock.nodes.${nodeName}.locked.narHash;
     }
   )
   { src = ./.; }
 ).defaultNix
+
