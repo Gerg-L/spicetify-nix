980209470ad7c5035e4444f2d91bc18dd048619c
Gerg-L <GregLeyda@proton.me>
2025-05-01 21:54:23 -0400

    test

--- modules/homeManager.nix
+++ modules/homeManager.nix
@@ -1,8 +1,22 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 {
-  home.packages = lib.mkIf config.programs.spicetify.enable config.programs.spicetify.createdPackages;
+  home = lib.mkIf config.programs.spicetify.enable {
+    packages = config.programs.spicetify.createdPackages;
+    activation = lib.mkIf pkgs.stdenv.isDarwin {
+      disableSpotifyUpdate =
+        let
+          updateDir = "${config.home.homeDirectory}/Library/Application Support/Spotify/PersistentCache/Update";
+        in
+        ''
+          run mkdir -p '${updateDir}' || echo "spicetify-nix: Failed to disable spotify updates"
+          run chflags -R uchg '${updateDir}' || echo "spicetify-nix: Failed to disable spotify updates"
+        '';
+    };
+  };
+
 }
