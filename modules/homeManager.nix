8c1be0e5e9a7f35ccd6f7b10bcfa08f2734dad91
Gerg-L <GregLeyda@proton.me>
2025-05-18 18:29:54 -0400

    Revert "test"
    This reverts commit 3b75a0348dea040e209bb9ca065436d8f192b14b.

--- modules/homeManager.nix
+++ modules/homeManager.nix
@@ -1,22 +1,8 @@
 {
   lib,
   config,
-  pkgs,
   ...
 }:
 {
-  home = {
-    packages = lib.mkIf config.programs.spicetify.enable config.programs.spicetify.createdPackages;
-    activation = lib.mkIf pkgs.stdenv.isDarwin {
-      disableSpotifyUpdate =
-        let
-          updateDir = "${config.home.homeDirectory}/Library/Application Support/Spotify/PersistentCache/Update";
-        in
-        ''
-          run mkdir -p '${updateDir}' || echo "spicetify-nix: Failed to disable spotify updates"
-          run chflags -R uchg '${updateDir}' || echo "spicetify-nix: Failed to disable spotify updates"
-        '';
-    };
-  };
-
+  home.packages = lib.mkIf config.programs.spicetify.enable config.programs.spicetify.createdPackages;
 }
